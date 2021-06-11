044e36ad286a56c209fe2d3bcbf156463aa2b007
Haruka.K <72332745+haruponponpopon@users.noreply.github.com>
2021-06-11 11:23:04 +0900

    Update vector_3D.cpp

--- array/vector_3D.cpp
+++ array/vector_3D.cpp
@@ -5,7 +5,7 @@ int main(){
 	int x = 3;
 	int y = 2;
 	int z = 4;
-	/*サイズx,y,zの三次元*/
-	vector<vector<vector<int>>> data(x, vector<vector<int>>(y, vector<int>(z)));
+	/*サイズx,y,zの三次元、3で初期化*/
+	vector<vector<vector<int>>> data(x, vector<vector<int>>(y, vector<int>(z, 3)));
 	cout << data[x-1][y-1][z-1] << endl;
 }
