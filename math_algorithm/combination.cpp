a116c430242c353fcda65033e2729f7cf3bcca4d
Haruka.K <72332745+haruponponpopon@users.noreply.github.com>
2021-06-19 20:13:35 +0900

    Create combination.cpp

---
+++ math_algorithm/combination.cpp
@@ -0,0 +1,12 @@
+30C1くらいなら計算できる  
+
+    ll combination(ll n, ll r) {
+      if ( r * 2 > n ) r = n - r;
+      ll dividend = 1;
+      ll divisor  = 1;
+      for ( unsigned int i = 1; i <= r; ++i ) {
+        dividend *= (n-i+1);
+        divisor  *= i;
+      }
+      return dividend / divisor;
+    }
