61493b190055728d3e918e7a38d6876e7d51d579
Haruka.K <72332745+haruponponpopon@users.noreply.github.com>
2021-06-19 20:25:18 +0900

    Update complex.cpp

--- math_algorithm/complex.cpp
+++ math_algorithm/complex.cpp
@@ -8,5 +8,8 @@ int main(){
     /*極座標(絶対値1,偏角pi/4の複素数を作る)*/
     complex<double> b = polar(1.0, M_PI/4.0);
     cout << b << endl;
-    cout << a*b << endl;
+    cout << a*b*2.0 << endl; //doubleでしか演算できなのので注意
+    /*おまけ 小数点以下の桁数の指定*/
+    double e = 20.33;
+    printf("%0.7f", e);
 }
