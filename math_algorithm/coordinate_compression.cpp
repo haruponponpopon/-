daa02b25eee4a63e7db0b3262101c1c535403ea8
Haruka.K <72332745+haruponponpopon@users.noreply.github.com>
2021-10-17 11:54:27 +0900

    Create coordinate_compression.cpp

---
+++ math_algorithm/coordinate_compression.cpp
@@ -0,0 +1,8 @@
+int comp(vector<int> &A){
+    std::map<int,int> mem;
+    for(auto p: A) mem[p] = 0;
+    int sz = 0;
+    for(auto &p: mem) p.second = sz++;
+    for(auto &p: A) p = mem[p];
+    return sz;
+}
