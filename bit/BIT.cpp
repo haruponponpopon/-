fc504902adce5faf4fa00c61847df07fa788f6c2
Haruka.K <72332745+haruponponpopon@users.noreply.github.com>
2022-06-12 11:12:45 +0900

    Update BIT.cpp

--- bit/BIT.cpp
+++ bit/BIT.cpp
@@ -60,7 +60,50 @@ public:
         }
     }
 };
-
+template <typename T>
+struct fenwick {
+    int n;
+    vector<T> node;
+ 
+    fenwick(int _n) : n(_n) {
+        node.resize(n);
+    }
+ 
+    void add(int x, T v) {
+        while (x < n) {
+            node[x] += v;
+            x |= (x + 1);
+        }
+    }
+ 
+    T get(int x) {  // [0, x]
+        T v = 0;
+        while (x >= 0) {
+            v += node[x];
+            x = (x & (x + 1)) - 1;
+        }
+        return v;
+    }
+ 
+    T get(int x, int y) {  // [x, y]
+        return (get(y) - (x ? get(x - 1) : 0));
+    }
+ 
+    int lower_bound(T v) {
+        int x = 0;
+        int h = 1;
+        while (n >= (h << 1)) {
+            h <<= 1;
+        }
+        for (int k = h; k > 0; k >>= 1) {
+            if (x + k <= n && node[x + k - 1] < v) {
+                v -= node[x + k - 1];
+                x += k;
+            }
+        }
+        return x;
+    }
+};
 int main(){
     int n=7;
     int a[5] = {3,1,5,4,2};
@@ -72,4 +115,5 @@ int main(){
         bit.add(a[i],1);
     }
     cout<<ans<<endl;
+    fenwick<int> row(n), col(n);
 }
