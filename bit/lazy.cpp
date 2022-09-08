03cf2c008d377e579a6a0107b0c6dc5319da9de0
Haruka.K <72332745+haruponponpopon@users.noreply.github.com>
2022-09-08 10:07:52 +0900

    Create lazy.cpp

---
+++ bit/lazy.cpp
@@ -0,0 +1,190 @@
+struct segtree {
+    using T = int;
+    using F = int;
+
+    T e() {
+        return (int) -2e9;
+    }
+
+    F id() {
+        return 0;
+    }
+
+    T op(T a, T b) {
+        return max(a, b);
+    }
+
+    T mapping(F f, T x) {
+        return f + x;
+    }
+
+    F composition(F f, F g) {
+        return f + g;
+    }
+
+    int n;
+    int size;
+    int log_size;
+    vector<T> node;
+    vector<F> lazy;
+
+    segtree() : segtree(0) {}
+    segtree(int _n) {
+        build(vector<T>(_n, e()));
+    }
+    segtree(const vector<T>& v) {
+        build(v);
+    }
+
+    void build(const vector<T>& v) {
+        n = (int) v.size();
+        if (n <= 1) {
+            log_size = 0;
+        } else {
+            log_size = 32 - __builtin_clz(n - 1);
+        }
+        size = 1 << log_size;
+        node.resize(2 * size, e());
+        lazy.resize(size, id());
+        for (int i = 0; i < n; i++) {
+            node[i + size] = v[i];
+        }
+        for (int i = size - 1; i > 0; i--) {
+            pull(i);
+        }
+    }
+
+    void push(int x) {
+        node[2 * x] = mapping(lazy[x], node[2 * x]);
+        node[2 * x + 1] = mapping(lazy[x], node[2 * x + 1]);
+        if (2 * x < size) {
+            lazy[2 * x] = composition(lazy[x], lazy[2 * x]);
+            lazy[2 * x + 1] = composition(lazy[x], lazy[2 * x + 1]);
+        }
+        lazy[x] = id();
+    }
+
+    void pull(int x) {
+        node[x] = op(node[2 * x], node[2 * x + 1]);
+    }
+
+    void set(int p, T v) {
+        assert(0 <= p && p < n);
+        p += size;
+        for (int i = log_size; i >= 1; i--) {
+            push(p >> i);
+        }
+        node[p] = v;
+        for (int i = 1; i <= log_size; i++) {
+            pull(p >> i);
+        }
+    }
+
+    T get(int p) {
+        assert(0 <= p && p < n);
+        p += size;
+        for (int i = log_size; i >= 1; i--) {
+            push(p >> i);
+        }
+        return node[p];
+    }
+	
+    T get(int l, int r) {
+        assert(0 <= l && l <= r && r <= n);
+        l += size;
+        r += size;
+        for (int i = log_size; i >= 1; i--) {
+            if (((l >> i) << i) != l) {
+                push(l >> i);
+            }
+            if (((r >> i) << i) != r) {
+                push((r - 1) >> i);
+            }
+        }
+        T vl = e();
+        T vr = e();
+        while (l < r) {
+            if (l & 1) {
+                vl = op(vl, node[l++]);
+            }
+            if (r & 1) {
+                vr = op(node[--r], vr);
+            }
+            l >>= 1;
+            r >>= 1;
+        }
+        return op(vl, vr);
+    }
+
+    void apply(int p, F f) {
+        assert(0 <= p && p < n);
+        p += size;
+        for (int i = log_size; i >= 1; i--) {
+            push(p >> i);
+        }
+        node[p] = mapping(f, node[p]);
+        for (int i = 1; i <= log_size; i++) {
+            pull(p >> i);
+        }
+    }
+	//[l,r)
+    void apply(int l, int r, F f) {
+        assert(0 <= l && l <= r && r <= n);
+        l += size;
+        r += size;
+        for (int i = log_size; i >= 1; i--) {
+            if (((l >> i) << i) != l) {
+                push(l >> i);
+            }
+            if (((r >> i) << i) != r) {
+                push((r - 1) >> i);
+            }
+        }
+        int ll = l;
+        int rr = r;
+        while (l < r) {
+            if (l & 1) {
+                node[l] = mapping(f, node[l]);
+                if (l < size) {
+                    lazy[l] = composition(f, lazy[l]);
+                }
+                l++;
+            }
+            if (r & 1) {
+                r--;
+                node[r] = mapping(f, node[r]);
+                if (l < size) {
+                    lazy[r] = composition(f, lazy[r]);
+                }
+            }
+            l >>= 1;
+            r >>= 1;
+        }
+        l = ll;
+        r = rr;
+        for (int i = 1; i <= log_size; i++) {
+            if (((l >> i) << i) != l) {
+                pull(l >> i);
+            }
+            if (((r >> i) << i) != r) {
+                pull((r - 1) >> i);
+            }
+        }
+    }
+};
+
+int main(){
+	vector<int>V(10);
+	segtree bit(V);
+	bit.apply(1,4,2);
+	bit.apply(3,5,1);
+	cout<<bit.get(1)<<endl;//2
+	cout<<bit.get(2)<<endl;//2
+	cout<<bit.get(3)<<endl;//3
+	cout<<bit.get(4)<<endl;//1
+	cout<<bit.get(5)<<endl;//0
+	cout<<bit.get(6)<<endl;//0
+	cout<<bit.get(1,3)<<endl;//[1,3)の最大値//2
+	cout<<bit.get(1,4)<<endl;//最大値を取得//3
+	cout<<bit.get(4,8)<<endl;//1
+}
