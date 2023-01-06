68b4d99c351bf65326bd17771d2e7f0d94bc34e8
Haruka.K <72332745+haruponponpopon@users.noreply.github.com>
2023-01-06 23:55:26 +0900

    Update UnionFindTree.cpp

--- search/UnionFindTree.cpp
+++ search/UnionFindTree.cpp
@@ -25,3 +25,47 @@ public:
         return -par[root(u)];
     }
 };
+
+struct dsu {
+    int n;
+    vector<int> p;
+    vector<int> sz;
+
+    dsu(int _n) : n(_n) {
+        p = vector<int>(n);
+        iota(p.begin(), p.end(), 0);
+        sz = vector<int>(n, 1);
+    }
+
+    inline int get(int x) {
+        if (p[x] == x) {
+            return x;
+        } else {
+            return p[x] = get(p[x]);
+        }
+    }
+
+    inline bool unite(int x, int y) {
+        x = get(x);
+        y = get(y);
+        if (x == y) {
+            return false;
+        }
+        p[x] = y;
+        sz[y] += sz[x];
+        return true;
+    }
+
+    inline bool same(int x, int y) {
+        return (get(x) == get(y));
+    }
+
+    inline int size(int x) {
+        return sz[get(x)];
+    }
+
+    inline bool root(int x) {
+        return (x == get(x));
+    }
+};
+https://atcoder.jp/contests/abc279/submissions/37770575
